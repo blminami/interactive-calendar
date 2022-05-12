e3265db789bed85232ef0d7f44ad2eca336c3596
Narcisa Minca <minca.narcisa@gmail.com>
2022-05-12 15:20:39 +0300

    remove history

--- .history/src/Config_20220511114053.ts
+++
@@ -1,9 +0,0 @@
-export const config = {
-  appId: '3895f47b-7727-4284-806b-624a9fca3145',
-  redirectUri: 'http://localhost:3000',
-  scopes: [
-    'user.read',
-    'mailboxsettings.read',
-    'calendars.readwrite'
-  ]
-};
--- .history/src/NavBar.d_20220512111312.ts
+++
@@ -1,9 +0,0 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'fds-sidenav': any;
-    'fds-app-bar': any;
-    'mwc-icon-button': any;
-    'fds-user-profile': any;
-    'fds-button': any;
-  }
-}
